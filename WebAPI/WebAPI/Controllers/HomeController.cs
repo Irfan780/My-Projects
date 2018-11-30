876a985c7ef79cca0b0f132583a8dc746644909d
Irfan780 <r.rahil780@gmail.com>
2018-11-29 23:03:16 -0800

    My APIs

---
+++ WebAPI/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public string  Contact(model1 m)
+        {
+
+            return m.client_id;
+        }
+
+        public class model1
+        {
+            
+            public string grant_type { get; set; }
+            public string username { get; set; }
+            public string client_id { get; set; }
+            public string client_secret { get; set; }
+            public string scope { get; set; }
+            
+
+        }
+    }
+}
